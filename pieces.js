73396b57d6900b69c9a714e9b122fd47479189b5
Corentin Braud <corentin.braud95@gmail.com>
2023-03-30 17:37:26 +0200

    Ajout filtre croissant/ décroissant et 2 filtres

--- pieces.js
+++ pieces.js
@@ -4,12 +4,13 @@ const pieces = await reponse.json();
 
 for (let i = 0; i < pieces.length; i++) {
 
+    // "article" équivaut à chacune des pièces en fonction de leur index
     const article = pieces[i];
     // RECUPERATION de l'élément du DOM qui accueillera les fiches
     const sectionFiches = document.querySelector(".fiches");
     // CREATION d’une balise dédiée à une pièce automobile
     const pieceElement = document.createElement("article");
-    // CREATION des balises 
+    // CREATION des balises et ajout des données depuis le fichier json
     const imageElement = document.createElement("img");
     imageElement.src = article.image;
 
@@ -30,17 +31,16 @@ for (let i = 0; i < pieces.length; i++) {
     
     // On RATTACHE la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
-    // On RATTACHE l’image à pieceElement (la balise article dans laquelle vont tous nos éléments créés)
+    // On RATTACHE les différentes parties à pieceElement
     pieceElement.appendChild(imageElement);
     pieceElement.appendChild(nomElement);
     pieceElement.appendChild(prixElement);
     pieceElement.appendChild(categorieElement);
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
-
 }
-
-// RECUPERATION bouton trier
+// Création bouton trier ordre croissant
+// RECUPERATION bouton trier dans l'ordre croissant
 const boutonTrier = document.querySelector(".btn-trier");
 // CREATION d'une nouvelle liste dans laquelle la fonction "sort" calcule et trie si un résultat entre 2 prix sont > = < à 0 
 boutonTrier.addEventListener("click", function () {
@@ -51,7 +51,18 @@ boutonTrier.addEventListener("click", function () {
      console.log(piecesOrdonnees);
 });
 
-// RECUPERATION bouton filtre
+// Création bouton trier ordre décroissant
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix; // Changement entre a.prix et b.prix
+     });
+     console.log(piecesOrdonnees);
+});
+
+// Création filtre pour les pièces de -35€
 const boutonFiltrer = document.querySelector(".btn-filtrer");
 // CREATION du filtre avec la fonction "filter"
 boutonFiltrer.addEventListener("click", function () {
@@ -69,7 +80,7 @@ for(let i = pieces.length -1 ; i >= 0; i--){
        noms.splice(i,1)
    }
 }
-console.log(noms)
+
 //CREATION de la liste pour accueillir les valeurs
 const abordablesElements = document.createElement('ul');
 //Ajout de chaque nom à la liste
@@ -81,3 +92,13 @@ for(let i=0; i < noms.length ; i++){
 // Ajout de l'en-tête puis de la liste au bloc résultats filtres
 document.querySelector('.abordables')
    .appendChild(abordablesElements)
+
+// Création bouton pieces sans description
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description
+    });
+    console.log(piecesFiltrees)
+});
